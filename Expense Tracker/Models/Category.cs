973ed9a231ade246000de541d2184ac781513080
shinonasada1907 <bonzed2001@gmail.com>
2022-12-04 11:17:46 +0700

    Chỉnh sửa Category

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -13,5 +13,13 @@ namespace Expense_Tracker.Models
         public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expence";
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
